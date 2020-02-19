6757f222f9c54bf350557b4b01ec44637ed8b0d7
ErickEJ <eriickoo@hotmail.com>
2020-02-19 17:17:10 -0600

    Revert "Merge pull request #2 from zauronhet/Luis"
    This reverts commit cd694011a94b002602c56268170a5b4e253cf2e9, reversing
    changes made to 025e921db06f305896080aa7b70c58cd69e0f035.

--- routes/web.php
+++ routes/web.php
@@ -11,39 +11,6 @@
 |
 */
 
-
-
-Route::get('/','ConsultasController@Index');
-Route::get('/Examenes','ConsultasController@Examenes');
-Route::get('/Intento_de_Examenes','ConsultasController@Intento_de_Examenes');
-Route::get('/Opciones_Preguntas_de_Examenes','ConsultasController@Opcion_Pregunta_de_Examen');
-Route::get('/Preguntas_de_Examenes','ConsultasController@Pregunta_de_Examen');
-Route::get('/Respuestas_por_Usuarios','ConsultasController@Respuesta_por_Usuario');
-Route::get('/Usuarios','ConsultasController@Usuario');
-
-
-
-
-
-
-
-
-
-
-Route::get('/1','Ex_Access_Controller@Access');
-//-------------------------------------------------------------------------------------------------
-Route::get('/Create','Ex_Create_User_Controller@New_User_Form');
-
-Route::post('/Create','Ex_Create_UserController@New_User_Insert')->name('user.Insert');
-
-Route::post('/UserName','INTSIAC_Create_UserController@UserNameGenerator')->name('user.UserName');
-//------------------------------------------------------------------------------------------------
-Route::get('/LogIn','INTSIAC_LogInController@LogIn');
-
-Route::post('/LogIn','INTSIAC_LogInController@Authentication')->name('user.LogIn');
-
-
-
-Route::get('/index','INTSIAC_Create_UserController@index');
-
-
+Route::get('/', function () {
+    return view('welcome');
+});
